53289e9443c985ee96be12a1ac5ad915285360e2
hardikothiya <hardikothiya@gmail.com>
2022-04-07 18:34:25 +0530

    Django admin has set up

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,13 +5,12 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    """User profile manger"""
+    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        """Create new user profile"""
-
+        """Create a new user profile"""
         if not email:
-            raise ValueError('Email is required')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
@@ -21,17 +20,15 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_superusr(self, name, email, password):
-        """create super user"""
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
-        user.is_staff=True
-
-        user.save()
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
-
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Required data for user"""
@@ -44,7 +41,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         return self.name
