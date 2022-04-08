53e9c24732ce9952b5ce8e4f0e0c2df493b28579
hardikothiya <hardikothiya@gmail.com>
2022-04-08 11:53:28 +0530

    APIView added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """ Hello Api"""
+
+    def get(self, request, format=None):
+
+        an_apiview = [
+        'Hy 1',
+        'hy 2',
+        'hy 3'
+        ]
+
+        return Response({'message': 'hello', 'an_apiview': an_apiview})
